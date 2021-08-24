edca2e487c4052c9d6af0987fcebc823e4b0d074
Junaid Ahmed <junaid@donesol.co.uk>
2021-08-24 23:13:56 +0500

    add AppNavbar using reactstrap and bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
+import AppNavbar from "./components/AppNavbar";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>MERN Shopping List App</h1>
+      <AppNavbar />
     </div>
   );
 }
